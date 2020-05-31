be97f99985cd320c83d608fa16d6e696f90b38b8
rhg <rhg@DESKTOP-3538QV7.localdomain>
2020-05-31 10:45:36 +0700

    update adb functionalities

---
+++ gacha_elper/adb.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+
+class Adb:
+    @classmethod
+    def connect(self, address, port):
+        subprocess.call(f'adb connect {address}:{port}'.split(' '))
+
+    @classmethod
+    def exec_out(self, cmd):
+        return subprocess.Popen(
+            f'adb exec-out {cmd}', stdout=subprocess.PIPE
+        ).communicate()[0]
+
+    @classmethod
+    def shell(self, cmd):
+        subprocess.call(f'adb shell {cmd}'.split(' '))
