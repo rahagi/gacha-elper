fd631f1756a2847809a96bb182d6178f8fd6065d
cytopz <cytopz@protonmail.com>
2021-04-01 10:26:20 +0700

    update example to 1.0

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # gacha-elper
 
-small utility to help automate repetitive tasks in gacha/grinding weaboo games.
+small utility to help automate repetitive tasks in gacha/grinding mobile (Android) video games.
 
 ## Usage
 
 ```python
-from src.gacha_elper import GachaElper as Elp
+from gacha_elper.elper import Elper as Elp
 ...
 ```
 see `examples`
--- examples/skadi_elper.py
+++ examples/skadi_elper.py
@@ -1,47 +1,54 @@
 from gacha_elper.elper import Elper as Elp
-from gacha_elper.elper import Point
+from gacha_elper.elper import Coordinate as Coord
 
 buttons = {
-    'home1': Point(215, 30),
-    'home2': Point(73, 230),
-    'back': Point(70, 35),
-    'combat_skadi': Point(927, 255),
-    'operation_map': Point(766, 385),
-    'gt_5': Point(682, 376),
-    'gt_6': Point(774, 282),
-    'auto_deploy': Point(919, 616),
-    'start_battle1': Point(913, 666),
-    'start_battle2': Point(888, 475)
+    "home1": Coord(215, 30),
+    "home2": Coord(73, 230),
+    "back": Coord(70, 35),
+    "combat_skadi": Coord(927, 255),
+    "operation_map": Coord(766, 385),
+    "gt_5": Coord(682, 376),
+    "gt_6": Coord(774, 282),
+    "auto_deploy": Coord(919, 616),
+    "start_battle1": Coord(913, 666),
+    "start_battle2": Coord(888, 475),
 }
 
+
 def start():
-    if not Elp.find('combat'):
-        print('going home')
-        Elp.tap(buttons['home1'])
-        Elp.tap(buttons['home2'])
-    Elp.tap(buttons['combat_skadi'])
-    Elp.wait(1.5)
-    Elp.tap(buttons['operation_map'])
+    elp = Elp(find_path="./assets")
+    if not elp.find("combat.png"):
+        print("going home")
+        elp.tap(buttons["home1"])
+        elp.tap(buttons["home2"])
+    elp.tap(buttons["combat_skadi"])
+    elp.wait(1.5)
+    elp.tap(buttons["operation_map"])
     gt_5 = True
     gt_5_count = 0
-    while True:
-        current_map = buttons['gt_5'] if gt_5 else buttons['gt_6']
-        Elp.tap(current_map)
-        if not Elp.find('auto_enabled'):
-            Elp.tap(buttons['auto_deploy'])
-        Elp.tap(buttons['start_battle1'], 2.5)
-        Elp.tap(buttons['start_battle2'])
-        Elp.wait(35)
-        while not Elp.find('trust'):
-            Elp.wait(5)
-        Elp.tap(Point(512, 512))
-        Elp.wait(7.5)
-        Elp.tap(buttons['back'])
-        if gt_5:
-            gt_5_count += 1
-        if gt_5 and gt_5_count == 2:
-            gt_5 = not gt_5
-            gt_5_count = 0
-        elif not gt_5:
-            gt_5 = not gt_5
+    try:
+        while True:
+            current_map = buttons["gt_5"] if gt_5 else buttons["gt_6"]
+            elp.tap(current_map)
+            if not elp.find("auto_enabled.png"):
+                elp.tap(buttons["auto_deploy"])
+            elp.tap(buttons["start_battle1"], 2.5)
+            elp.tap(buttons["start_battle2"])
+            elp.wait(35)
+            elp.wait_until_find("trust.png")
+            elp.tap(Coord(512, 512))
+            elp.wait(7.5)
+            elp.tap(buttons["back"])
+            if gt_5:
+                gt_5_count += 1
+            if gt_5 and gt_5_count == 2:
+                gt_5 = not gt_5
+                gt_5_count = 0
+            elif not gt_5:
+                gt_5 = not gt_5
+    except KeyboardInterrupt:
+        print("done.")
 
+
+if __name__ == "__main__":
+    start()
