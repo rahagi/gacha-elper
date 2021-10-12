7fcb6fbb8649d5be2a3d4f55fa7ebee909ae5137
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2021-10-12 21:43:52 +0700

    lint

--- gacha_elper/adb/adb.py
+++ gacha_elper/adb/adb.py
@@ -16,18 +16,19 @@ class Adb:
         if not cls.list_devices():
             raise AdbError("No emulator/device")
         try:
-            return subprocess.Popen(
-                cmd.split(" "), stdout=subprocess.PIPE
-            ).communicate()[0]
+            with subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE) as pipe:
+                return pipe.communicate()[0]
         except FileNotFoundError:
             raise AdbError("adb is not found in your PATH") from None
 
     @classmethod
     def list_devices(cls) -> List[Optional[str]]:
+        """
+        Run `adb devices`
+        """
         cmd = "adb devices"
-        devices = subprocess.Popen(
-            cmd.split(" "), stdout=subprocess.PIPE
-        ).communicate()[0]
+        with subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE) as pipe:
+            devices = pipe.communicate()[0]
 
         return [
             x.replace("\t", " ")
@@ -36,10 +37,16 @@ class Adb:
 
     @classmethod
     def connect(cls, address: str, port: str):
+        """
+        Run `adb connect`
+        """
         cmd = f"adb connect {address}:{port}"
         return cls.__run_cmd(cmd)
 
     @classmethod
     def exec_out(cls, cmd: str):
+        """
+        Run `adb exec-out`
+        """
         cmd = f"adb exec-out {cmd}"
         return cls.__run_cmd(cmd)
